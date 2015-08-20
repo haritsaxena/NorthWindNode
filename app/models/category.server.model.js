d913ee419b05367cf5841540107575bf0acfdca1
Harit Saxena <Harit.Saxena@gmail.com>
2015-08-20 22:22:30 +0530

    added category

---
+++ app/models/category.server.model.js
@@ -0,0 +1,37 @@
+'use strict';
+
+/**
+ * Module dependencies.
+ */
+var mongoose = require('mongoose'),
+	Schema = mongoose.Schema;
+
+function validateLength(v){
+	return v.length <= 15;
+}
+/**
+ * Category Schema
+ */
+var CategorySchema = new Schema({
+	// Category model fields   
+	// ...
+	created : {
+		type : Date,
+		default : Date.now
+	},
+	name : {
+		type: String,
+		default : '',
+		trim : true,
+		unique : true,
+		required : 'name is mandatory',
+		validate : [validateLength, 'name must be less than 15 chars']
+	},
+	description : {
+		type : String,
+		default  : '',
+		trim : true
+	}
+});
+
+mongoose.model('Category', CategorySchema);
